3ee90b129366944b6f8dc4407670902ab616b958
Michael Breen <8309342+michael-db@users.noreply.github.com>
2022-05-31 14:08:21 +0100

    get-version.sh: improve, use commit ID as fallback

--- get-version.sh
+++ get-version.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 # Copyright (c) 2022 Michael Breen (https://mbreen.com)
-# Output a program source-version string derived from the git state.
-# If the working directory corresponds to a version tag that has
-# been pushed to origin then output "ORIGIN-URL VERSION", e.g.,
-# "https://example.com/my-repo v1.2".
+# Output a "SOURCE VERSION" string derived from the git state.
+# SOURCE:
+# If the working directory corresponds to a commit that has been
+# pushed to origin then SOURCE is the origin URL, otherwise
+# SOURCE is the local directory.
+# VERSION:
+# If there are uncommitted changes then VERSION is "devel".
+# Otherwise, if the working directory corresponds to a commit
+# with a version tag in SOURCE (i.e., the tag has been pushed
+# if SOURCE is origin) then it is the tag.
+# Otherwise, VERSION is the currently checked-out commit ID.
 set -e
 
-TAG=$( (git describe --tags --match='v[0-9]*' --dirty 2>/dev/null \
-    || echo -dirty)|sed s/.*-dirty/devel/)
+COMMIT=$(git describe --dirty --long --always |
+    sed 's/.*-g//;s/.*-dirty/devel/')
 
-SOURCE=$( (2>/dev/null git ls-remote --tags origin "$TAG" \
-    || >&2 echo "$0: Failed to check origin repo," \
-    "using working dir as version source.") |
-    grep -q . && (git ls-remote --get-url origin |
-    sed -E 's,^git@([^:]*):/*(.*)$,https://\1/\2,') || pwd)
+SOURCE=$(git rev-list --glob remotes/origin | grep -q ^$COMMIT &&
+    git ls-remote --get-url origin || true)
 
-echo "$SOURCE $TAG"
+TAG=$( (git describe --tags --match='v[0-9]*' --exact-match \
+    --dirty 2>/dev/null || echo -dirty) | sed 's/.*dirty.*//' )
+
+if [ -n "$TAG" ]; then
+    if [ -z "$SOURCE" ] ||
+            ( (2>/dev/null git ls-remote --tags origin "$TAG" ||
+            >&2 echo "$0: failed to check origin repo for tag $TAG") |
+            grep -q . ) then
+        COMMIT="$TAG"
+    fi
+fi
+
+# Normalize URL.
+SOURCE=$(echo $SOURCE|sed 's,^git@github.com:,https://github.com/,')
+
+[ -n "$SOURCE" ] || SOURCE=$(pwd)
+
+echo "$SOURCE $COMMIT"
